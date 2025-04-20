cec688889d3e6510b09748622c4551e531f591eb
charnimo <charnimoatez@gmail.com>
2025-04-20 21:03:58 +0100

    gg

---
+++ electron/main.js
@@ -0,0 +1,42 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+import isDev from 'electron-is-dev';
+import { fileURLToPath } from 'url';
+import { enable } from '@electron/remote/main';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  enable(mainWindow.webContents);
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+
+  if (isDev) mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
